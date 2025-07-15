1fd2b3b977a528d82293d7b644b92013ca110a67
MacTec2025 <maclembo@gmail.com>
2025-07-15 09:09:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import NatureNewsApp from './NatureNewsApp';
-<img src="/earthrise-logo.svg" className="app-logo" alt="EarthRise logo" />
 
 function App() {
   return (
     <div className="App">
       <header className="app-header">
-        <img src={logo} className="app-logo" alt="EarthRise logo" />
+        <img src="/earthrise-logo.svg" className="app-logo" alt="EarthRise logo" />
         <h1>EarthRise News</h1>
         <p className="tagline">Curated stories from the natural world</p>
       </header>
