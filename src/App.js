9e7d715a528539d82dbc17ef1fbc943a07aa7e21
MacTec2025 <maclembo@gmail.com>
2025-07-15 18:34:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css'; 
+import './App.css';
+import Navbar from './components/Navbar';
 import NatureNewsApp from './NatureNewsApp';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <NatureNewsApp />
     </div>
   );
